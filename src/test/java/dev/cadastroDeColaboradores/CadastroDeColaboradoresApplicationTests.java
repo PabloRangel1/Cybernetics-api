f140ec8eed3dba4dbe618be5c39644632c8b7c9b
pablorangel1 <pbmx10games@gmail.com>
2025-04-07 20:01:03 -0300

    bug: Corrigindo diretorio de src

---
+++ src/test/java/dev/cadastroDeColaboradores/CadastroDeColaboradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cadastroDeColaboradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeColaboradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
