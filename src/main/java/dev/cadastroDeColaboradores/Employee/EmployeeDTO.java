3f5c6771d8d4905970ca7ff0997f038ea60be4cc
pablorangel1 <pbmx10games@gmail.com>
2025-05-05 16:05:52 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no EmployeeMapper os caminhos entidade x dto

--- src/main/java/dev/cadastroDeColaboradores/Employee/EmployeeDTO.java
+++ src/main/java/dev/cadastroDeColaboradores/Employee/EmployeeDTO.java
@@ -1,4 +1,18 @@
 package dev.cadastroDeColaboradores.Employee;
 
+import dev.cadastroDeColaboradores.Tarefas.TarefasModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String cargo;
+    private TarefasModel tarefas;
+    private String departamento;
 }
--- src/main/java/dev/cadastroDeColaboradores/Employee/EmployeeMapper.java
+++ src/main/java/dev/cadastroDeColaboradores/Employee/EmployeeMapper.java
@@ -1,4 +1,32 @@
 package dev.cadastroDeColaboradores.Employee;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmployeeMapper {
+
+    public EmployeeModel map(EmployeeDTO employeeDTO) {
+
+        EmployeeModel employeeModel = new EmployeeModel();
+        employeeModel.setId(employeeDTO.getId());
+        employeeModel.setNome(employeeDTO.getNome());
+        employeeModel.setEmail(employeeDTO.getEmail());
+        employeeModel.setCargo(employeeDTO.getCargo());
+        employeeModel.setTarefas(employeeDTO.getTarefas());
+        employeeModel.setDepartamento(employeeDTO.getDepartamento());
+
+        return employeeModel;
+    }
+
+    public EmployeeDTO map(EmployeeModel employeeModel) {
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        employeeDTO.setId(employeeModel.getId());
+        employeeDTO.setNome(employeeModel.getNome());
+        employeeDTO.setEmail(employeeModel.getEmail());
+        employeeDTO.setCargo(employeeModel.getCargo());
+        employeeDTO.setTarefas(employeeModel.getTarefas());
+        employeeDTO.setDepartamento(employeeModel.getDepartamento());
+        return employeeDTO;
+    }
+
 }
